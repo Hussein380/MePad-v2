783bc49e1f5064853f036837b1f18cc10888ddea
Hussein Yussuf Garane <huznigarane@gmail.com>
2025-01-23 08:59:30 +0300

    Update db.js

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         console.log('Attempting to connect to MongoDB...');
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
 
         mongoose.connection.on('error', err => {
@@ -28,4 +25,4 @@ const connectDB = async () => {
     }
 };
 
-module.exports = connectDB; 
+module.exports = connectDB;
